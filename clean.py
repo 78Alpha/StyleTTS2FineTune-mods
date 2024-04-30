07c4ecae910590a06acc308b5f48b907c36ba1b2
78Alpha <37676834+78Alpha@users.noreply.github.com>
2024-04-30 15:52:23 -0700

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,12 +1,14 @@
 import os
 import glob
 
-trainingData = glob.glob("/mnt/q/Utilities/CUDA/StyleTTS2FineTune/trainingdata/*.txt")
-srt = glob.glob("/mnt/q/Utilities/CUDA/StyleTTS2FineTune/srt/*.*")
-silencedAudio = glob.glob("/mnt/q/Utilities/CUDA/StyleTTS2FineTune/silencedAudio/*.wav")
-segmentedAudio = glob.glob("/mnt/q/Utilities/CUDA/StyleTTS2FineTune/segmentedAudio/*.wav")
-paddedAudio = glob.glob("/mnt/q/Utilities/CUDA/StyleTTS2FineTune/paddedAudio/*.wav")
-badAudio = glob.glob("/mnt/q/Utilities/CUDA/StyleTTS2FineTune/badAudio/*.wav")
+main_dir = os.getcwd()
+
+trainingData = glob.glob(f"{main_dir}/trainingdata/*.txt")
+srt = glob.glob(f"{main_dir}/srt/*.*")
+silencedAudio = glob.glob(f"{main_dir}/silencedAudio/*.wav")
+segmentedAudio = glob.glob(f"{main_dir}/segmentedAudio/*.wav")
+paddedAudio = glob.glob(f"{main_dir}/paddedAudio/*.wav")
+badAudio = glob.glob(f"{main_dir}/badAudio/*.wav")
 
 cleanupDir = [trainingData, srt, silencedAudio, segmentedAudio, paddedAudio, badAudio]
 
